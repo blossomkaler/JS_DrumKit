ba097ad269cae6f3b9b44d73f62be275e5fdffec
Blossom <blossomkaler@gmail.com>
2023-03-10 17:51:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const keys= Array.from(document.querySelectorAll('.key'));
+
+document.addEventListener('keydown',playSound);
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+
+function playSound(e){
+  const audio =document.querySelector(`audio[data-key = "${e.keyCode}"]`) ;
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  
+  if (!audio) return;
+  key.classList.add('playing');
+  audio.currentTime =0;
+  audio.play();
+}
+
+function removeTransition(e){
+  if(e.propertyName !== 'transform') return; 
+  e.target.classList.remove('playing');
+
+}
